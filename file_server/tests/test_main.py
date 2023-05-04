2e4fa8ea34a79d3beccda8a1e5801d73f4dac5a2
Gleb Rysev <glebrysev@gmail.com>
2023-05-04 19:07:49 +0100

    Tests for likes completed

--- file_server/tests/test_main.py
+++ file_server/tests/test_main.py
@@ -3,7 +3,7 @@ import mongomock
 import io
 from unittest.mock import patch
 import requests
-from file_server import main
+import main
 
 from .helpers import generate_access_token
 
@@ -22,7 +22,7 @@ def access_header():
 
 #Test function to test the upload_file function
 def test_upload_file_1(client, access_header):
-    file = "../test.jpg"
+    file = "test.jpg"
     data = {
         'file': (open(file, 'rb'), file),
         'tags' : "meow cat cute",
@@ -35,7 +35,7 @@ def test_upload_file_1(client, access_header):
 
 #Test function to test the upload_file function if tags are missing
 def test_upload_file_2(client, access_header):
-    file = "../test.jpg"
+    file = "test.jpg"
     data = {
         'file': (open(file, 'rb'), file),
         'owner' : "111"
@@ -47,7 +47,7 @@ def test_upload_file_2(client, access_header):
 
 #Test function to test the upload_file function if owner is missing
 def test_upload_file_3(client, access_header):
-    file = "../test.jpg"
+    file = "test.jpg"
     data = {
         'file': (open(file, 'rb'), file),
         'tags' : "meow cat cute"
@@ -56,6 +56,41 @@ def test_upload_file_3(client, access_header):
     
     assert response.status_code == 400
     assert response.json['error'] == "Missing fields"
+
+#Test function for like_image function
+def test_like_image_1(client, access_header):
+    file = "test.jpg"
+    data = {'file': (open(file, 'rb'), file), 'tags' : "meow cat cute", 'owner' : "111"}
+    client.post('/images/upload', data=data, content_type='multipart/form-data', headers=access_header)
+    data = {'image_id': 0}
+    response = client.post('/images/like', data=data, headers=access_header)
+    
+    assert response.status_code == 200
+
+#Test function for like_image function
+def test_like_image_2(client, access_header):
+    data = {'image_id': 0}
+    response = client.post('/images/like', data=data, headers=access_header)
+    
+    assert response.status_code == 404
+
+#Test function for the like/unlike flow
+def test_like_image_3(client, access_header):
+    file = "test.jpg"
+    data = {'file': (open(file, 'rb'), file), 'tags' : "meow cat cute", 'owner' : "111"}
+    client.post('/images/upload', data=data, content_type='multipart/form-data', headers=access_header)
+    data = {'image_id': 0}
+    response = client.post('/images/unlike', data=data, headers=access_header)
+    assert response.status_code == 401
+
+    response = client.post('/images/like', data=data, headers=access_header)
+    assert response.status_code == 200
+
+    response = client.post('/images/unlike', data=data, headers=access_header)
+    assert response.status_code == 200 
+
+    response = client.post('/images/unlike', data=data, headers=access_header)
+    assert response.status_code == 401
 
 #Test function to test get_image function
 def test_get_image_1(client, access_header):
@@ -71,7 +106,7 @@ def test_get_image_2(client, access_header):
 
 #Test function to test get_image_by_id function
 def test_get_image_byid_1(client, access_header):
-    file = "../test.jpg"
+    file = "test.jpg"
     data = {'file': (open(file, 'rb'), file), 'tags' : "meow cat cute", 'owner' : "111"}
     client.post('/images/upload', data=data, content_type='multipart/form-data', headers=access_header)
     response = client.get('/images/id/0', headers=access_header)
@@ -86,7 +121,7 @@ def test_get_image_byid_2(client, access_header):
 
 # Testing get_user_images function
 def test_get_user_images_1(client, access_header):
-    file = "../test.jpg"
+    file = "test.jpg"
     data = [
         {'file': (open(file, 'rb'), file), 'tags' : "meow cat cute", 'owner' : "111"},
         {'file': (open(file, 'rb'), file), 'tags' : "dog meme funny", 'owner' : "111"},
@@ -110,7 +145,7 @@ def test_get_user_images_1(client, access_header):
 
 # Testing get_images_by_tag function
 def test_get_images_by_tag(client, access_header):
-    file = "../test.jpg"
+    file = "test.jpg"
     data = [
         {'file': (open(file, 'rb'), file), 'tags' : "meow cat cute", 'owner' : "111"},
         {'file': (open(file, 'rb'), file), 'tags' : "dog meme funny", 'owner' : "111"},
